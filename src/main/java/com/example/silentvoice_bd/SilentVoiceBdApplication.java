899ec16fb15348eb96c1f357c483806b2898b90e
sayad-dot <sayadkhan0555@gmail.com>
2025-06-29 11:23:08 +0600

    initial commit

---
+++ src/main/java/com/example/silentvoice_bd/SilentVoiceBdApplication.java
@@ -0,0 +1,13 @@
+package com.example.silentvoice_bd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SilentVoiceBdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SilentVoiceBdApplication.class, args);
+    }
+
+}
